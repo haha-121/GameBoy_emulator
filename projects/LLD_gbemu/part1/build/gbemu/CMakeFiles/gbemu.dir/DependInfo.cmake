ffb1a1aedee092665ede371aaf43d4f4e3801f8a
haha-121 <z0976302623@gmail.com>
2023-09-18 19:00:52 +0800

    新增

---
+++ projects/LLD_gbemu/part1/build/gbemu/CMakeFiles/gbemu.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/projects/LLD_gbemu/part1/gbemu/main.c" "gbemu/CMakeFiles/gbemu.dir/main.c.o" "gcc" "gbemu/CMakeFiles/gbemu.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vboxuser/projects/LLD_gbemu/part1/build/lib/CMakeFiles/emu.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ projects/LLD_gbemu/part1/build/lib/CMakeFiles/emu.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/bus.c" "lib/CMakeFiles/emu.dir/bus.c.o" "gcc" "lib/CMakeFiles/emu.dir/bus.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/cart.c" "lib/CMakeFiles/emu.dir/cart.c.o" "gcc" "lib/CMakeFiles/emu.dir/cart.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/cpu.c" "lib/CMakeFiles/emu.dir/cpu.c.o" "gcc" "lib/CMakeFiles/emu.dir/cpu.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/cpu_fetch.c" "lib/CMakeFiles/emu.dir/cpu_fetch.c.o" "gcc" "lib/CMakeFiles/emu.dir/cpu_fetch.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/cpu_proc.c" "lib/CMakeFiles/emu.dir/cpu_proc.c.o" "gcc" "lib/CMakeFiles/emu.dir/cpu_proc.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/cpu_util.c" "lib/CMakeFiles/emu.dir/cpu_util.c.o" "gcc" "lib/CMakeFiles/emu.dir/cpu_util.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/dbg.c" "lib/CMakeFiles/emu.dir/dbg.c.o" "gcc" "lib/CMakeFiles/emu.dir/dbg.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/dma.c" "lib/CMakeFiles/emu.dir/dma.c.o" "gcc" "lib/CMakeFiles/emu.dir/dma.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/emu.c" "lib/CMakeFiles/emu.dir/emu.c.o" "gcc" "lib/CMakeFiles/emu.dir/emu.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/gamepad.c" "lib/CMakeFiles/emu.dir/gamepad.c.o" "gcc" "lib/CMakeFiles/emu.dir/gamepad.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/instructions.c" "lib/CMakeFiles/emu.dir/instructions.c.o" "gcc" "lib/CMakeFiles/emu.dir/instructions.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/interrupts.c" "lib/CMakeFiles/emu.dir/interrupts.c.o" "gcc" "lib/CMakeFiles/emu.dir/interrupts.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/io.c" "lib/CMakeFiles/emu.dir/io.c.o" "gcc" "lib/CMakeFiles/emu.dir/io.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/lcd.c" "lib/CMakeFiles/emu.dir/lcd.c.o" "gcc" "lib/CMakeFiles/emu.dir/lcd.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/ppu.c" "lib/CMakeFiles/emu.dir/ppu.c.o" "gcc" "lib/CMakeFiles/emu.dir/ppu.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/ppu_pipeline.c" "lib/CMakeFiles/emu.dir/ppu_pipeline.c.o" "gcc" "lib/CMakeFiles/emu.dir/ppu_pipeline.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/ppu_sm.c" "lib/CMakeFiles/emu.dir/ppu_sm.c.o" "gcc" "lib/CMakeFiles/emu.dir/ppu_sm.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/ram.c" "lib/CMakeFiles/emu.dir/ram.c.o" "gcc" "lib/CMakeFiles/emu.dir/ram.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/stack.c" "lib/CMakeFiles/emu.dir/stack.c.o" "gcc" "lib/CMakeFiles/emu.dir/stack.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/timer.c" "lib/CMakeFiles/emu.dir/timer.c.o" "gcc" "lib/CMakeFiles/emu.dir/timer.c.o.d"
+  "/home/vboxuser/projects/LLD_gbemu/part1/lib/ui.c" "lib/CMakeFiles/emu.dir/ui.c.o" "gcc" "lib/CMakeFiles/emu.dir/ui.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
